326f9a6de522fa5c356ee14cec51ec33ba76f719
Zach Bray <54759665+zachbray9@users.noreply.github.com>
2021-08-30 14:40:49 -0400

    Zork 1.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,20 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine().Trim().ToUpper();
+            if(inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if(inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
