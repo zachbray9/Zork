21ce6abfff274f0da883c18dcb26ce466611dbee
Zachary Bray <zachbray9@knights.ucf.edu>
2021-09-08 18:30:04 -0400

    Zork.1.2
    Finished version of Zork 1.2. Refactored ToCommand function.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,39 +8,15 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine().Trim().ToUpper();
+            string inputString = Console.ReadLine().Trim();
             Commands command = ToCommand(inputString);
-
-            if(inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if(inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+            Console.WriteLine(command);
 
         }
 
         static Commands ToCommand(string commandString)
         {
-            Commands command;
-
-            if(commandString = "QUIT")
-            {
-                command = Commands.QUIT;
-            }
-            else
-            {
-                command = Commands.UNKNOWN;
-            }
-
-            return command;
-
+            return Enum.TryParse<Commands>(commandString, true, out Commands command) ? command : Commands.UNKNOWN;
         }
     }
 }
