4487dfa09d9027ecbb9a5dce4804c3ae77000caa
Zachary Bray <zachbray9@knights.ucf.edu>
2021-09-08 19:24:16 -0400

    Zork 1.3
    Added the game loop and a switch statement indicating what each command does.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,16 +7,43 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+           
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
 
-            string inputString = Console.ReadLine().Trim();
-            Commands command = ToCommand(inputString);
-            Console.WriteLine(command);
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.NORTH:                       
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
 
+                Console.WriteLine(outputString);
+            }
         }
+
+
 
         static Commands ToCommand(string commandString)
         {
-            return Enum.TryParse<Commands>(commandString, true, out Commands command) ? command : Commands.UNKNOWN;
+            return Enum.TryParse<Commands>(commandString, ignoreCase: true, out Commands command) ? command : Commands.UNKNOWN;
         }
+
     }
 }
