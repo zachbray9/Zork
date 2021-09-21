3e8c751fcaee0ef7ee4393072343eeeee3cd188c
Zachary Bray <zachbray9@knights.ucf.edu>
2021-09-21 19:35:57 -0400

    Zork 3.1
    Added a Room class which gave each room a unique name and description. Changed the 2d array from an array of strings to an array of Rooms.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,23 +4,35 @@ namespace Zork
 {
     class Program
     {
-        private static readonly string[,] Rooms = {
-            {"Rocky Trail", "South of House", "Canyon View" },
-            {"Forest", "West of House", "Behind House" },
-            {"Dense Woods", "North of House", "Clearing" }
+        private static readonly Room[,] Rooms = {
+            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
         private static (int row, int column) Location = (1, 1);
 
+        public static Room CurrentRoom
+        {
+
+            get 
+            {
+                return Rooms[Location.row, Location.column];
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
+            InitializeRoomDescriptions();
 
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
-                Console.Write($"{Rooms[Location.row, Location.column]}\n> ");
+                Console.Write($"{CurrentRoom}\n> ");                                                                                        
                 command = ToCommand(Console.ReadLine().Trim());
 
                 string outputString;
@@ -31,7 +43,7 @@ namespace Zork
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.";
+                        outputString = CurrentRoom.Description;
                         break;
 
                     case Commands.NORTH:
@@ -86,6 +98,21 @@ namespace Zork
             }
 
             return didMove;
+        }
+
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0,0].Description = "You are on a rock-strewn trail.";                                                                                 //Rocky Trail
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";         //South of House
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";                                                      //Canyon View
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";                                                     //Forest
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                        //West of House
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";     //Behind House
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";            //Dense Woods
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";         //North of House
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";                                    //Clearing
         }
 
 
---
+++ Zork/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+    public class Room
+    {
+        public string Name { get; }
+        public string Description { get; set; }
+
+        //Overrides the ToString method that is inherited from System.Object when trying to use Console.Write(CurrentRoom) because CurrentRoom isn't a string.
+        public override string ToString() => Name;                    
+
+        public Room(string name, string description = "")
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
