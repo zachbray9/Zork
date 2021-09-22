b339e6c00fe2e6c68773dd108dfa62cb7d5b2359
Zachary Bray <zachbray9@knights.ucf.edu>
2021-09-22 18:49:40 -0400

    Zork 3.2
    Added an associative array (Dictionary) to initialize the descriptions of the rooms instead of hard coding them. Also implemented the auto look feature which automatically calls the LOOK command, but only at the beginning of the game and if a move was made.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
@@ -25,6 +26,7 @@ namespace Zork
 
         static void Main(string[] args)
         {
+            Room previousRoom = null; 
             InitializeRoomDescriptions();
 
             Console.WriteLine("Welcome to Zork!");
@@ -32,7 +34,15 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
-                Console.Write($"{CurrentRoom}\n> ");                                                                                        
+
+                Console.WriteLine(CurrentRoom);   
+                if(previousRoom != CurrentRoom)
+                {
+                    Console.WriteLine(CurrentRoom.Description);
+                    previousRoom = CurrentRoom;
+                }
+                Console.Write("> ");
+
                 command = ToCommand(Console.ReadLine().Trim());
 
                 string outputString;
@@ -102,17 +112,23 @@ namespace Zork
 
         private static void InitializeRoomDescriptions()
         {
-            Rooms[0,0].Description = "You are on a rock-strewn trail.";                                                                                 //Rocky Trail
-            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";         //South of House
-            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";                                                      //Canyon View
+            var roomMap = new Dictionary<string, Room>();
 
-            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";                                                     //Forest
-            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                        //West of House
-            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";     //Behind House
+            foreach(Room room in Rooms)
+            {
+                roomMap[room.Name] = room;
+            }
 
-            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";            //Dense Woods
-            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";         //North of House
-            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";                                    //Clearing
+            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
+            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
+            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
+            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+
         }
 
 
